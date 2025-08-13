f702a36e8431bdffdcd85fc473ac0374433dbc55
jyeon03 <bjlyeon2003@naver.com>
2025-08-13 20:03:56 +0900

    bug: 파일 확장자 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Signin from "./pages/auth/Signin";
-import SigninSeller from "./pages/auth/SigninSeller";
-import Signup from "./pages/auth/Signup";
-import SignupSeller from "./pages/auth/SignupSeller";
-import TestPage from "./pages/common/TestPage";
-import MainPage from "./pages/main/MainPage";
-import MainPageSeller from "./pages/main/MainPageSeller";
+import Signin from "./pages/auth/Signin.jsx";
+import SigninSeller from "./pages/auth/SigninSeller.jsx";
+import Signup from "./pages/auth/Signup.jsx";
+import SignupSeller from "./pages/auth/SignupSeller.jsx";
+import TestPage from "./pages/common/TestPage.jsx";
+import MainPage from "./pages/main/MainPage.jsx";
+import MainPageSeller from "./pages/main/MainPageSeller.jsx";
 
-import StoreRegistration from "./pages/auth/StoreRegistration";
-import StoreDocumentUpload from "./pages/auth/StoreDocumentUpload";
+import StoreRegistration from "./pages/auth/StoreRegistration.jsx";
+import StoreDocumentUpload from "./pages/auth/StoreDocumentUpload.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
