f9e3476773a9669d4a2ea3dadccd429fb108c9b1
jyeon03 <bjlyeon2003@naver.com>
2025-08-20 01:11:29 +0900

    feat: 추천상품 페이지

---
+++ src/pages/products/RecommendedPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ProductListPage from "../../components/common/products/ProductListPage";
+import { mockUtils } from "../../mocks/UnifiedMockData";
+
+const getRecommendedProducts = () => {
+  const products = mockUtils.getRecommendedProducts();
+
+  return products.map((product) => ({
+    id: product.id,
+    storeName: mockUtils.getStoreById(product.storeId)?.name || "상점",
+    productName: product.name,
+    categoryName:
+      mockUtils.getCategoryById(product.categoryId)?.name || "카테고리",
+    originalPrice: product.originalPrice,
+    discountPrice: product.discountPrice,
+    imageUrl: product.images?.[0] || "",
+    isLiked: mockUtils.isProductLiked(1, product.id),
+    expiryTime: Date.now() + Math.random() * 7 * 24 * 60 * 60 * 1000,
+  }));
+};
+
+const RecommendedPage = () => {
+  return (
+    <ProductListPage
+      title="추천 상품"
+      getProducts={getRecommendedProducts}
+      showExpiry={true}
+      showCategory={true}
+      description="테스트 사용자님이 좋아하실 재고를 발견했어요!"
+    />
+  );
+};
+
+export default RecommendedPage;
